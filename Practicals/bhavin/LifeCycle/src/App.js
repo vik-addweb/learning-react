26be3ac47790b91927aa62637abd3b9dae26aa7f
snehal.addweb <snehal.addweb@gamil.com>
2019-04-10 13:03:34 +0530

    Implemented LifeCycale

---
+++ Practicals/bhavin/LifeCycle/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import LifeCycale from './component/LifeCycleA';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LifeCycale />
+      </div>
+    );
+  }
+}
+
+export default App;
